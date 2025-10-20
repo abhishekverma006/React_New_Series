0fc62aa340c1fc83fb7e80c4ed15fd78b9aae1aa
AbhishekVerma006 <abhishekverma84951@gmail.com>
2025-10-21 04:56:13 +0530

    created 2 type of setup

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, This is a AKV Royal Code</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Hello, This is AKV Royal Coding</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
